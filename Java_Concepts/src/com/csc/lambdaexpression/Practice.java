0554a961c958026940211a2b76d7c96cc5b77e93
roudressh <rhbuddy1@gmail.com>
2024-12-30 10:43:26 +0530

    add it

---
+++ Java_Concepts/src/com/csc/lambdaexpression/Practice.java
@@ -0,0 +1,10 @@
+package com.csc.lambdaexpression;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("hello git");
+	}
+
+}
